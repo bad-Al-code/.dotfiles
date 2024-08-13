43e2598e93a306030c4e92236372aecf2c81a576
badAl <badalsingh120403@gmail.com>
2024-08-13 16:20:08 +0530

    feat: add script to build and install Neovim from source

--- bash.sh
+++ bash.sh
@@ -7,29 +7,6 @@ LOG_FILE="$HOME/dotfiles_update.log"
 DATE_TIME=$(date +"%Y-%m-%d %H:%M:%S")
 APT_OPTIONS="-y"
 
-PACKAGES=(
-  vim
-  curl
-  wget
-  p7zip-full
-  ffmpeg
-  tmux
-  build-essential
-  i3
-  i3status
-  software-properties-common
-  nitrogen
-  btop
-  obs-studio
-  tree
-  usb-creator-gtk
-  cmake
-  gpg
-  xclip
-  picom
-  htop
-)
-
 log() {
   echo "$DATE_TIME - $SCRIPT_NAME: $1" | tee -a "$LOG_FILE"
 }
@@ -61,6 +38,30 @@ update_system() {
 
 update_system
 
+PACKAGES=(
+  vim
+  curl
+  wget
+  p7zip-full
+  ffmpeg
+  tmux
+  build-essential
+  i3
+  i3status
+  software-properties-common
+  nitrogen
+  btop
+  obs-studio
+  tree
+  usb-creator-gtk
+  cmake
+  gpg
+  xclip
+  picom
+  htop
+  git
+)
+
 install_packages() {
   log "Starting package installation..."
 
@@ -84,3 +85,86 @@ install_packages() {
 }
 
 install_packages
+
+# Neovim
+NEOVIM_DIR="$HOME/neovim"
+NEOVIM_REPO="https://github.com/neovim/neovim.git"
+
+DEPENDENCIES=(
+  ninja-build
+  gettext
+  libtool
+  libtool-bin
+  autoconf
+  automake
+  cmake
+  g++
+  pkg-config
+  unzip
+  curl
+  doxygen
+)
+
+install_dependencies() {
+  log "Installing dependencies for building Neovim..."
+
+  if apt update $APT_OPTIONS; then
+    log "Package list updated successfully."
+  else
+    log "Failed to update package list."
+    exit 1
+  fi
+
+  for dependency in "${DEPENDENCIES[@]}"; do
+    if apt install $APT_OPTIONS "$dependency"; then
+      log "Dependency '$dependency' installed successfully."
+    else
+      log "Failed to install dependency '$dependency'."
+      exit 1
+    fi
+  done
+
+  log "All dependencies installed successfully."
+}
+
+clone_neovim_repo() {
+  log "Cloning Neovim repository..."
+
+  if [[ -d "$NEOVIM_DIR" ]]; then
+    log "Neovim directory already exists. Removing it..."
+    rm -rf "$NEOVIM_DIR"
+  fi
+
+  if git clone "$NEOVIM_REPO" "$NEOVIM_DIR"; then
+    log "Neovim repository cloned successfully."
+  else
+    log "Failed to clone Neovim repository."
+    exit 1
+  fi
+}
+
+build_and_install_neovim() {
+  log "Building Neovim from source..."
+
+  cd "$NEOVIM_DIR" || exit
+
+  if make CMAKE_BUILD_TYPE=Release; then
+    log "Neovim built successfully."
+  else
+    log "Failed to build Neovim."
+    exit 1
+  fi
+
+  if make install; then
+    log "Neovim installed successfully."
+  else
+    log "Failed to install Neovim."
+    exit 1
+  fi
+}
+
+install_dependencies
+clone_neovim_repo
+build_and_install_neovim
+
+log "Neovim has been built and installed successfully."
